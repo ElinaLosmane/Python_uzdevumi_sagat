92807c6c897db913d7abc15284885eb189b82560
Elina <elina.losmane@grobinasskola.lv>
2023-11-03 10:49:44 +0200

    stundas darbs 2

--- 2uzd.py
+++ 2uzd.py
@@ -1,4 +1,19 @@
 """ 
 Iegūt ziņas, izmantojot RSS plūsmu no google.lv.
-
+1)RSS? Jaunumi un saturs; satura agregators - sistēma, kas ļauj abonēt interesējošos avotus
+2)plūsmu no google.lv
 """
+import feedparser
+
+# URL uz RSS plūsmu
+rss_url='https://www.lsm.lv/laika-zinas/:lv'
+
+# iegūstam RSS plūsmas datus un veicam analīzi
+kkk=feedparser.parse(rss_url)
+
+# noformēšana...
+for entry in kkk.entries:
+    print("Virsraksts", entry.title)
+    print("Saite:", entry.link)
+    print("Publicēšanas datums", entry.published)
+    print("\n")
